a3600dd27e211d7840508e8d981f4435c8fd9da7
tegnike <user@mbp.local>
2019-07-26 13:41:35 +0200

    rspec system test

---
+++ spec/system/site_layout_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'SiteLayoutTest', type: :system do
+
+    it 'layout links' do
+      visit root_path
+      expect(response.body).to have_link 'Home'
+    end
+
+
+end
