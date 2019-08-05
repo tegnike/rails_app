28c055a9b972d34c6da459249a28659703e49ba9
tegnike <user@mbp.local>
2019-08-05 18:29:05 +0200

    add account activation

---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe UserMailer, type: :mailer do
+  describe "account_activation" do
+    let(:user) { create(:user, name: "Example", email: "user@example.com") }
+    let(:mail) { UserMailer.account_activation(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Account activation")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["noreply@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match(user.name)
+      expect(mail.body.encoded).to match(user.activation_token)
+      expect(mail.body.encoded).to match(CGI.escape(user.email))
+    end
+  end
+
+  # 第12章で実装
+  # describe "password_reset" do
+  #   let(:mail) { UserMailer.password_reset }
+  #
+  #   it "renders the headers" do
+  #     expect(mail.subject).to eq("Password reset")
+  #     expect(mail.to).to eq(["to@example.org"])
+  #     expect(mail.from).to eq(["from@example.com"])
+  #   end
+  #
+  #   it "renders the body" do
+  #     expect(mail.body.encoded).to match("Hi")
+  #   end
+  # end
+
+end
---
+++ spec/requests/users_signup_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe "UsersSignup", type: :request do
+  describe "アクティブ機能付きのサインアップを確認する" do
+    before {
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+      @user = assigns(:user)
+    }
+    context "有効化していない状態でログインを試した場合" do
+      before { log_in_as(@user) }
+      it "ログインできないこと" do
+        expect(is_logged_in?).to be_falsey
+      end
+    end
+    context "有効化トークンが不正な場合" do
+      before { get edit_account_activation_path("invalid token", email: @user.email) }
+      it "ログインできないこと" do
+        expect(is_logged_in?).to be_falsey
+      end
+    end
+    context "トークンは正しいがメールアドレスが無効な場合" do
+      before { get edit_account_activation_path(@user.activation_token, email: 'wrong') }
+      it "ログインできないこと" do
+        expect(is_logged_in?).to be_falsey
+      end
+    end
+    context "有効化トークンが正しい場合" do
+      before { get edit_account_activation_path(@user.activation_token, email: @user.email) }
+      it "ログインできること" do
+        expect(is_logged_in?).to be_truthy
+        expect(response).to redirect_to user_path(@user)
+      end
+    end
+  end
+  describe "ActionMailer::Base.deliveriesを確認する" do
+    before {
+      ActionMailer::Base.deliveries.clear
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    }
+    it "配信されたメッセージが1つであること" do
+      expect(ActionMailer::Base.deliveries.size).to eq 1
+    end
+  end
+end
