0c77c937f29ee53d022b0ad1aa24abad16ae2d0d
tegnike <user@mbp.local>
2019-08-09 16:20:41 +0200

    add password reset

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "account_activation" do
-    let(:user) { create(:user, name: "Example", email: "user@example.com") }
+    let(:user) { create(:user, name: "Example", email: "user@example.com", activation_token: User.new_token) }
     let(:mail) { UserMailer.account_activation(user) }
 
     it "renders the headers" do
@@ -18,19 +18,20 @@ RSpec.describe UserMailer, type: :mailer do
     end
   end
 
-  # 第12章で実装
-  # describe "password_reset" do
-  #   let(:mail) { UserMailer.password_reset }
-  #
-  #   it "renders the headers" do
-  #     expect(mail.subject).to eq("Password reset")
-  #     expect(mail.to).to eq(["to@example.org"])
-  #     expect(mail.from).to eq(["from@example.com"])
-  #   end
-  #
-  #   it "renders the body" do
-  #     expect(mail.body.encoded).to match("Hi")
-  #   end
-  # end
+  describe "password_reset" do
+    let(:user) { create(:michael, name: "Example", email: "user@example.com", reset_token: User.new_token) }
+    let(:mail) { UserMailer.password_reset(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Password reset")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["noreply@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match(user.reset_token)
+      expect(mail.body.encoded).to match(CGI.escape(user.email))
+    end
+  end
 
 end
---
+++ spec/requests/password_reset_spec.rb
@@ -0,0 +1,140 @@
+require 'rails_helper'
+
+RSpec.describe "PasswordReset", type: :request do
+
+  describe "パスワード再設定用のメール送信をテストする" do
+    let!(:user) { create(:michael)}
+    context "メールアドレスが無効の場合" do
+      before { post password_resets_path, params: { password_reset: { email: "" } } }
+      it "エラーメッセージが表示されること" do
+        expect(response.body).to include 'alert-danger'
+      end
+    end
+    context "メールアドレスが有効な場合" do
+      before {
+        ActionMailer::Base.deliveries.clear
+        post password_resets_path, params: { password_reset: { email: user.email } }
+      }
+      it "reset_digestが更新されていること" do
+        expect(user.reset_digest).not_to eq(user.reload.reset_digest)
+      end
+      it "配信されたメッセージが1つであること" do
+        expect(ActionMailer::Base.deliveries.size).to eq 1
+      end
+      it "root_urlにリダイレクトされること" do
+        expect(response.body).not_to include 'alert-danger'
+        expect(response).to redirect_to root_url
+      end
+    end
+  end
+
+  describe "メールリンクをテストする" do
+    let!(:user) { create(:michael)}
+    before {
+      post password_resets_path, params: { password_reset: { email: user.email } }
+      @user = assigns(:user)
+    }
+    context "メールアドレスが無効の場合" do
+      before { get edit_password_reset_path(@user.reset_token, email: "") }
+      it "root_urlにリダイレクトされること" do
+        expect(response).to redirect_to root_url
+      end
+    end
+    context "無効なユーザーの場合" do
+      before {
+        @user.toggle!(:activated)
+        get edit_password_reset_path(@user.reset_token, email: @user.email)
+      }
+      it "root_urlにリダイレクトされること" do
+        expect(response).to redirect_to root_url
+      end
+    end
+    context "メールアドレスが有効で、トークンが無効な場合" do
+      before { get edit_password_reset_path('wrong token', email: @user.email) }
+      it "root_urlにリダイレクトされること" do
+        expect(response).to redirect_to root_url
+      end
+    end
+    context "メールアドレスもトークンも有効な場合" do
+      before { get edit_password_reset_path(@user.reset_token, email: @user.email) }
+      it "リクエストが成功すること" do
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe "パスワード再設定フォームをテストする" do
+    let!(:user) { create(:michael)}
+    before {
+      post password_resets_path, params: { password_reset: { email: user.email } }
+      @user = assigns(:user)
+    }
+    context "無効なパスワードの場合" do
+      before {
+        patch password_reset_path(@user.reset_token),
+          params: { email: @user.email,
+                    user: { password:              "foobaz",
+                            password_confirmation: "barquux" } }
+      }
+      it "エラーメッセージが表示されること" do
+        expect(response.body).to include 'error_explanation'
+      end
+    end
+    context "パスワードが空の場合" do
+      before {
+        patch password_reset_path(@user.reset_token),
+          params: { email: @user.email,
+                    user: { password:              "",
+                            password_confirmation: "" } }
+      }
+      it "エラーメッセージが表示されること" do
+        expect(response.body).to include 'error_explanation'
+      end
+    end
+    context "パスワードが有効な場合" do
+      before {
+        patch password_reset_path(@user.reset_token),
+          params: { email: @user.email,
+                    user: { password:              "foobaz",
+                            password_confirmation: "foobaz" } }
+      }
+      it "ログインすること" do
+        expect(is_logged_in?).to be_truthy
+        expect(response).to redirect_to @user
+      end
+    end
+  end
+
+  describe "パスワード再設定の期限切れをテストする" do
+    let!(:user) { create(:michael)}
+    before {
+      post password_resets_path, params: { password_reset: { email: user.email } }
+      @user = assigns(:user)
+    }
+    context "発行3時間後に再設定を試みた場合" do
+      before {
+        @user.update_attribute(:reset_sent_at, 3.hours.ago)
+        patch password_reset_path(@user.reset_token),
+          params: { email: @user.email,
+                    user: { password:              "foobaz",
+                            password_confirmation: "foobaz" } }
+      }
+      it "new_password_reset_pathにリダイレクトされること" do
+        expect(response).to redirect_to new_password_reset_path
+      end
+    end
+    context "発行1時間後に再設定を試みた場合" do
+      before {
+        @user.update_attribute(:reset_sent_at, 1.hours.ago)
+        patch password_reset_path(@user.reset_token),
+          params: { email: @user.email,
+                    user: { password:              "foobaz",
+                            password_confirmation: "foobaz" } }
+      }
+      it "ログインすること" do
+        expect(is_logged_in?).to be_truthy
+        expect(response).to redirect_to @user
+      end
+    end
+  end
+end
